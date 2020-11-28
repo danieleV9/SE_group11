b6ad0e302ec5405036c835f21113c34df42d0947
Daniele Valentino <58222709+danieleV9@users.noreply.github.com>
2020-11-28 21:26:07 +0100

    login e home page mvc

---
+++ JavaApplication7/src/model/MaintainerModel.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import dao.MaintainerDAO;
+
+/**
+ *
+ * @author lyuba
+ */
+public class MaintainerModel {
+    private String username;
+    private String password;
+
+    public MaintainerModel(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public MaintainerModel findMaintainer(String username, String password, String role) throws Exception{
+        MaintainerDAO dao = new MaintainerDAO();
+        MaintainerModel ad = dao.findMaintainer(username, password, role);
+        return ad;
+    }
+    
+}
