eefef0e39c3086018e61268486797fd88798a1b0
jennifercutolo <jenni.cutolo@gmail.com>
2020-12-01 16:31:21 +0100

    aggiunta delle seguenti classi:
    AdminSkillView.java, AdminSkillView.form, AdminSkillController.java , SkillModel.java

---
+++ Progetto_SE_group11/src/mainProva/Prova.java
@@ -0,0 +1,51 @@
+package mainProva;
+
+
+import controller.AdminHomeController;
+import controller.LoginController;
+import controller.PlannerCreateController;
+import controller.PlannerHomeController;
+import java.util.List;
+import model.AdminModel;
+import model.MaintainerModel;
+import model.MaintenanceActivityModel;
+import model.PlannerModel;
+import view.AdminHomeView;
+import view.LoginView;
+import view.PlannerCreateView;
+import view.PlannerHomeView;
+import view.UsersListView;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dava9
+ */
+public class Prova {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginView login = new LoginView();
+        UsersListView users = new UsersListView();
+        AdminModel admin = new AdminModel("","");
+        MaintainerModel maintainer = new MaintainerModel("","");
+        PlannerModel planner = new PlannerModel("","");
+        LoginController contr = new LoginController(login,admin,planner,maintainer);
+        
+        PlannerHomeView phv = new PlannerHomeView();
+        PlannerModel pm = new PlannerModel("","");
+        PlannerHomeController phc = new PlannerHomeController(phv,pm);
+        PlannerCreateView pcv = new PlannerCreateView();
+        MaintenanceActivityModel mam = new MaintenanceActivityModel(0,0,"","","");
+        PlannerCreateController pcc = new PlannerCreateController(pcv,pm);
+    }
+    
+}
